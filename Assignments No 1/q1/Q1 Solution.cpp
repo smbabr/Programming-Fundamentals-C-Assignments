b1fd783c1c2b05400e1aa867f2f52f9ceafa86cb
smbabr <162433741+smbabr@users.noreply.github.com>
2024-10-20 13:51:34 +0500

    Q1 Solution.cpp

--- Assignments No 1/q1/Q1 Solution.cpp
+++ Assignments No 1/q1/Q1 Solution.cpp
@@ -2,9 +2,19 @@
 
 int main() {
     int num;
-    printf("Enter an integer: ");
+    
+    // Let's make it fun! Ask for a number like a magician!
+    printf("Enter a number to see its magical transformation: ");
+    
+    // Taking input using scanf
     scanf("%d", &num);
-    printf("Square: %d\n", num * num);
+    
+    // Calculating the square of the number in a creative way
+    int square = num * num;
+    
+    // Displaying the result with a little bit of fun!
+    printf("Behold! The square of %d is %d. Magic, isn't it?\n", num, square);
+    
     return 0;
 }
 
