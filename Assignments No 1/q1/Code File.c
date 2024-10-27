59f58af34de3466019900967e4641f38362332bf
SMBabar-Ali <162433741+smbabr@users.noreply.github.com>
2024-10-27 20:49:04 +0500

    Add files via upload

---
+++ Assignments No 1/q1/Code File.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int a, b, sum;
+
+    
+    printf("Enter the first integer: ");
+    scanf("%d", &a);
+    
+    printf("Enter the second integer: ");
+    scanf("%d", &b);
+    
+    
+    sum = a + b;
+    
+    
+    printf("The sum of %d and %d is %d\n", a, b, sum);
+
+    return 0;
+}
